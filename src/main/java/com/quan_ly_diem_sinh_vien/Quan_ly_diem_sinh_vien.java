96d5725c951c16f31d574bc011389aebbe29187f
Deocomate <deocomate@GMAIL.COM>
2023-12-22 14:51:24 +0700

    Update in main

--- src/main/java/com/quan_ly_diem_sinh_vien/Quan_ly_diem_sinh_vien.java
+++ src/main/java/com/quan_ly_diem_sinh_vien/Quan_ly_diem_sinh_vien.java
@@ -13,15 +13,8 @@ public class Quan_ly_diem_sinh_vien {
     public static void main(String[] args) {
         System.out.println("Chao Long");
         System.out.println("Chao Duy Dat");
-        
-<<<<<<< HEAD
-        
         System.out.println("Chao Long 2");
-        
-        
-=======
         System.out.println("Chao Khoaa!!");
->>>>>>> main
         
     }
 }
