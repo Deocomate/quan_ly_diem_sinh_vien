1c77fcc63c8c32d4d03b6943ae1b97d23d449be7
Deocomate <deocomate@GMAIL.COM>
2023-12-22 14:39:49 +0700

    Update

--- src/main/java/com/quan_ly_diem_sinh_vien/Quan_ly_diem_sinh_vien.java
+++ src/main/java/com/quan_ly_diem_sinh_vien/Quan_ly_diem_sinh_vien.java
@@ -12,6 +12,7 @@ public class Quan_ly_diem_sinh_vien {
 
     public static void main(String[] args) {
         System.out.println("Chao Long");
+        System.out.println("Chao Duy Dat");
         
     }
 }
