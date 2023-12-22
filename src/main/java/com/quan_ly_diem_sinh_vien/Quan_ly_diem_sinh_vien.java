9044de2e76ea183b0e26a0a135cd1674a18db416
Deocomate <deocomate@GMAIL.COM>
2023-12-22 13:45:15 +0700

    First cmt

---
+++ src/main/java/com/quan_ly_diem_sinh_vien/Quan_ly_diem_sinh_vien.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.quan_ly_diem_sinh_vien;
+
+/**
+ *
+ * @author Admin
+ */
+public class Quan_ly_diem_sinh_vien {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
